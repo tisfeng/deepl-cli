15e5fd1f342c1eb28bb61097cd68c6b9e1322b4f
tisfeng <tisfeng@gmail.com>
2022-07-18 17:41:30 +0800

    feat: support for specifying the source and target languages for translation

--- deepL.py
+++ deepL.py
@@ -1,43 +1,114 @@
 import random
-import sys
 import time
 
+import click
 import requests
 
 
-def deepl_translator(translateText, sourceLanguage, targetLanguage):
-    translateText = '"' + translateText + '"'
-    sourceLanguage = '"' + sourceLanguage + '"'
-    targetLanguage = '"' + targetLanguage + '"'
-    data = '{"jsonrpc":"2.0","method": "LMT_handle_jobs","params":{"jobs":[{"kind":"default","raw_en_sentence":' + translateText + ',"raw_en_context_before":[],"raw_en_context_after":[],"preferred_num_beams":4,"quality":"fast"}],"lang":{"user_preferred_langs":["EN","ZH"],"source_lang_computed":"auto","target_lang":"EN"},"priority":-1,"commonJobParams":{},"timestamp":' + str(int(time.time() * 10000)) + '},"id":' + str(random.randint(1, 100000000)) + '}'
+# Use click to define the command line interface.
+@click.command()
+@click.argument("text")
+@click.option("--source_language", default="auto", help="Source language")
+@click.option("--target_language", default="ZH", help="Target language")
+def deepLTranslate(text, source_language, target_language):    
+    print(f"{source_language} --> {target_language}: {text}")
+    print()
+    
+    text = '"' + text + '"'
+    source_language = '"' + source_language + '"'
+    target_language = '"' + target_language + '"'
 
-    data = '{"jsonrpc":"2.0","method": "LMT_handle_jobs","params":{"jobs":[{"kind":"default","sentences":[{"text": ' + translateText + ', "id": 0, "prefix": ""}],"raw_en_context_before":[],"raw_en_context_after":[],"preferred_num_beams":4}],"lang":{"preference":{"weight": {},"default": "default"},"source_lang_computed":' + sourceLanguage + ',"target_lang":' + targetLanguage + '},"priority":1,"commonJobParams":{ "browserType": 1, "formality": null },"timestamp":' + str(
+    data = '{"jsonrpc":"2.0","method": "LMT_handle_jobs","params":{"jobs":[{"kind":"default","sentences":[{"text": ' + text + ', "id": 0, "prefix": ""}],"raw_en_context_before":[],"raw_en_context_after":[],"preferred_num_beams":4}],"lang":{"preference":{"weight": {},"default": "default"},"source_lang_computed":' + source_language + ',"target_lang":' + target_language + '},"priority":1,"commonJobParams":{ "browserType": 1, "formality": null },"timestamp":' + str(
         int(time.time() * 10000)) + '},"id":' + str(
             random.randint(1, 100000000)) + '}'
-
-
-    r = requests.post('https://www2.deepl.com/jsonrpc',
-                      headers={'content-type': 'application/json'},
-                      data=data.encode())
     # print("data:" + data)
 
-    # print new line
-    print()
-
-    print(r.text)
-
-    return r.json()['result']['translations']
-    return r.json()['result']['translations'][0]['beams']
+    result = requests.post('https://www2.deepl.com/jsonrpc',
+                      headers={'content-type': 'application/json'},
+                      data=data.encode()).json()
+    
+    if 'error' in result:
+        print("error:", result.json()['error'])
+        return   
+    
+    
+    # print("result:", result)
+    beams = result['result']['translations'][0]['beams']
+    translations = []    
+    for beam in beams:
+        translation = beam['sentences'][0]['text']
+        print(translation)
+        translations.append(translation)
+        
+    return translations
 
   
-if __name__ == '__main__':
-	# print(deepl_translator('摸鱼就开心'))
+if __name__ == '__main__':    
+    deepLTranslate()
 
-    # get params from deepl.py
-    translateText = sys.argv[1]
-    sourceLanguage = sys.argv[2] # EN
-    targetLanguage = sys.argv[3] # ZH
-    print(deepl_translator(translateText, sourceLanguage, targetLanguage))
 
-# python3 deepl.py good EN ZH
-# python3 deepl.py 开心 ZH EN
+#==================================== Params ==================================== 
+"""
+{
+  "jsonrpc": "2.0",
+  "method": "LMT_handle_jobs",
+  "params": {
+    "jobs": [
+      {
+        "kind": "default",
+        "sentences": [
+          {
+            "text": "good",
+            "id": 0,
+            "prefix": ""
+          }
+        ],
+        "raw_en_context_before": [],
+        "raw_en_context_after": [],
+        "preferred_num_beams": 4
+      }
+    ],
+    "lang": {
+      "preference": {
+        "weight": {},
+        "default": "default"
+      },
+      "source_lang_computed": "EN",
+      "target_lang": "ZH"
+    },
+    "priority": -1,
+    "commonJobParams": {
+      "browserType": 1,
+      "formality": null
+    },
+    "timestamp": 1657606963196
+  },
+  "id": 11000045
+}
+"""
+
+
+#==================================== Result ==================================== 
+"""
+{
+  "jsonrpc": "2.0",
+  "id": 23856610,
+  "result": {
+    "translations": [
+      {
+        "beams": [
+          { "sentences": [{ "text": "很好", "ids": [0] }], "num_symbols": 4 },
+          { "sentences": [{ "text": "不错", "ids": [0] }], "num_symbols": 3 },
+          { "sentences": [{ "text": "好", "ids": [0] }], "num_symbols": 3 },
+          { "sentences": [{ "text": "好的", "ids": [0] }], "num_symbols": 4 }
+        ],
+        "quality": "normal"
+      }
+    ],
+    "target_lang": "ZH",
+    "source_lang": "EN",
+    "source_lang_is_confident": "False",
+    "detectedLanguages": {}
+  }
+}
+"""
